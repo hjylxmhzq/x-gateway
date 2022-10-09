43b218375383442287f72e780b9af495b7158d42
root <root@localhost.localdomain>
2022-10-09 14:49:55 +0800

    feat: change startup script

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-pm2 start ./packages/gateway/dist/main.js --node-args="--es-module-specifier-resolution=node"  
+pm2 start ./packages/gateway/dist/main.js --node-args="--es-module-specifier-resolution=node" --name=x-gateway

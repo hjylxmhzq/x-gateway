28d40ba6747612b044b0ec32287327e59dc1f724
root <root@localhost.localdomain>
2022-10-11 15:29:45 +0800

    feat: add geoip

--- packages/gateway/src/middlewares/logger.ts
+++ packages/gateway/src/middlewares/logger.ts
@@ -6,7 +6,7 @@ import geoip from 'geoip-lite';
 function requestLogger() {
   return (async (ctx: Koa.Context, next: Koa.Next) => {
     const realIp = requestIp.getClientIp(ctx.request);
-    const geo = geoip.lookup(realIp);
+    const geo = geoip.lookup(realIp || '');
     const geoInfo = geo ? `${geo.country}/${geo.region}/${geo.city}` : 'unknown ip location';
     logger.info(`${ctx.request.method.toUpperCase()} ${realIp}(${geoInfo}) ${ctx.request.path}`);
     await next();

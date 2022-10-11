452e2b29b37e7ac2c14a5d8834c0ae2297aaa297
hjylxmhzq <hujingyuan25@163.com>
2022-10-11 02:24:42 -0500

    feat: add geoip

--- packages/gateway/src/middlewares/logger.ts
+++ packages/gateway/src/middlewares/logger.ts
@@ -1,11 +1,14 @@
 import Koa from 'koa';
 import { logger } from '../utils/logger';
 import requestIp from 'request-ip';
+import geoip from 'geoip-lite';
 
 function requestLogger() {
   return (async (ctx: Koa.Context, next: Koa.Next) => {
     const realIp = requestIp.getClientIp(ctx.request);
-    logger.info(`${ctx.request.method.toUpperCase()} ${realIp} ${ctx.request.path}`);
+    const geo = geoip.lookup(realIp);
+    const geoInfo = geo ? `${geo.country}/${geo.region}/${geo.city}` : 'unknown ip location';
+    logger.info(`${ctx.request.method.toUpperCase()} ${realIp}(${geoInfo}) ${ctx.request.path}`);
     await next();
   });
 }

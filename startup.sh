9e9aed48fc9bcdff6216a9fff94b30a61f7a54b5
Tony Hu <hujingyuan25@163.com>
2022-12-09 16:30:31 +0800

    fix: startup script

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 pm2 delete x-gateway
-pm2 start ./packages/gateway/dist/main.js --name x-gateway --node-args="--es-module-specifier-resolution=node"
+pm2 start ./packages/gateway/dist/main.js --name x-gateway --node-args="--experimental-loader=./packages/gateway/nodejs/loader.js"
